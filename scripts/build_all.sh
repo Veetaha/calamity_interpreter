eb8390a431df5176db3548be4ba0184c479c39bf
Tegeran <gerzoh1@gmail.com>
2018-07-10 16:26:29 +0300

    travis try #5

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
-set -ev
-buildDir="cmake-build-debug"
+set -e
+BILD_DIR="cmake-build-debug"
 cd ..
-if [ ! -d "${buildDir}" ]
+if [ ! -d "${BILD_DIR}" ]
 then
-    echo "No ${buildDir} was found, rebuilding calamity_lexer from scratch"
-    mkdir "$buildDir"
-    cd "$buildDir"
-    cmake ..
+    echo "No ${BILD_DIR} was found, rebuilding calamity_lexer from scratch"
+    mkdir "$BILD_DIR"
+    cd "$BILD_DIR"
+    cmake -DCMAKE_C_COMPILER=/usr/bin/cc -DCMAKE_CXX_COMPILER=/usr/bin/c++ ..
     cd ..
 fi
-    cd "$buildDir"/lexertl_gen
+    cd "$BILD_DIR"/lexertl_gen
     make
 #    wait $!
-    ./lexertl_gen.out ../../include/lexertl_gen2.h
+    ./lexertl_gen.out ../../include/lexertl_gen.h
     cd ..
     make
